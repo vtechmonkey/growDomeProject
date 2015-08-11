1eb788a7286001d58f3e0865c5c318b19753506b
Sarah <sarah.oconnor@webinnovate.ie>
2015-08-11 02:21:42 +0100

    version 0.0.2

--- config/env/production.js
+++ config/env/production.js
@@ -24,8 +24,8 @@ module.exports = {
 		js: 'public/dist/application.min.js'
 	},
 	facebook: {
-		clientID: process.env.FACEBOOK_ID || 'APP_ID',
-		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+		clientID: process.env.FACEBOOK_ID || '1448228805484017',
+		clientSecret: process.env.FACEBOOK_SECRET || '94a1c89e87629b9b1286ef3051dba26b',
 		callbackURL: '/auth/facebook/callback'
 	},
 	twitter: {
